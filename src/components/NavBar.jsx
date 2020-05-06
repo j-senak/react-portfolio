9b2559c1b7a4e13e0fcbcdc56333833af9af65e9
JohnS <senakj2011@hotmail.com>
2020-05-06 19:25:45 -0400

    added NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,35 @@ import React from 'react';
 const NavBar = () => {
     return (
         <div>
-            
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="index.html" id="logo">John Senak</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="contact.html">Contact</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="portfolio.html">Portfolio</a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="index.html">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
         </div>
     );
 };
