063d7c04fae45416d88d8e4cb2174175f1499c5b
JohnS <senakj2011@hotmail.com>
2020-05-06 19:21:57 -0400

    forgot to change to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer class="footer">
-        <div class="two-toned-footer-color"></div>
-        <p class="text-muted text-muted-footer text-center">&copy; Copyright</p>
+      <footer className="footer">
+        <div className="two-toned-footer-color"></div>
+        <p className="text-muted text-muted-footer text-center">&copy; Copyright</p>
       </footer>
     </div>
   );
