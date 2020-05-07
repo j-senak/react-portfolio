96bd876b8b5ba66cede69be66a2ccfb86e28e852
JohnS <senakj2011@hotmail.com>
2020-05-06 23:28:25 -0400

    App function and cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Portfolio from "./containers/Portfolio/Portfolio";
+import About from "./containers/About/About";
+import Contact from "./containers/Contact/Contact";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
+
+const App = () =>  {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path = "/" component = {About} />
+        <Route exact path = "About" component = {About} />
+        <Route exact path = "/project" component = {Portfolio} />
+        <Route exact path = "/contact" component = {Contact} />
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
+
 
 export default App;
